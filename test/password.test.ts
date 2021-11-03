0ff1e1d0a7b812acce66013d9b18c0b7fa3cee47
Johan Eliasson <johan@nhost.io>
2021-11-03 19:52:37 +0100

    reset password tests

--- test/password.test.ts
+++ test/password.test.ts
@@ -75,3 +75,65 @@ test('change existing password', async () => {
   expect(error).toBeNull();
   expect(session).toBeTruthy();
 });
+
+test('reset password', async () => {
+  // TODO: Put in a helper function
+  // create and verify user
+  const email = faker.internet.email().toLocaleLowerCase();
+  const password = faker.internet.password(8);
+
+  // sign up
+  await auth.signUp({
+    email,
+    password,
+  });
+
+  // get email that was sent
+  const message = await mailhog.latestTo(email);
+
+  if (!message?.html) {
+    throw new Error('email does not exists');
+  }
+
+  // get verify email link
+  const verifyEmailLink = htmlUrls({ html: message.html }).find(
+    (href: { value: string; url: string; uri: string }) => {
+      return href.url.includes('verifyEmail');
+    }
+  );
+
+  // verify email
+  await axios.get(verifyEmailLink.url, {
+    maxRedirects: 0,
+    validateStatus: (status) => {
+      return status === 302;
+    },
+  });
+
+  // reset password
+  const { error } = await auth.resetPassword({ email });
+
+  expect(error).toBeNull();
+
+  // get email that was sent
+  const messageResetPassword = await mailhog.latestTo(email);
+
+  if (!messageResetPassword?.html) {
+    throw new Error('email does not exists');
+  }
+
+  // get verify email link
+  const resetPasswordLink = htmlUrls({ html: messageResetPassword.html }).find(
+    (href: { value: string; url: string; uri: string }) => {
+      return href.url.includes('passwordReset');
+    }
+  );
+
+  // verify email
+  await axios.get(resetPasswordLink.url, {
+    maxRedirects: 0,
+    validateStatus: (status) => {
+      return status === 302;
+    },
+  });
+});
