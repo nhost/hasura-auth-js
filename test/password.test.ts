53c725409ad681a97f4ba7571e540b940336c9a5
Johan Eliasson <johan@nhost.io>
2021-11-03 22:46:24 +0100

    test cleanup

--- test/password.test.ts
+++ test/password.test.ts
@@ -1,53 +1,14 @@
-import { HasuraAuthClient } from '../src/index';
 import faker from 'faker';
-import createMailhogClient from 'mailhog';
 import axios from 'axios';
+import { auth, mailhog, signUpAndInUser, signUpAndVerifyUser } from './helpers';
 
 const htmlUrls = require('html-urls');
 
-const AUTH_BACKEND_URL = 'http://localhost:1337/v1/auth';
-
-const auth = new HasuraAuthClient({
-  url: AUTH_BACKEND_URL,
-});
-
-const mailhog = createMailhogClient({
-  host: 'localhost',
-  port: 8025,
-});
-
 test('change existing password', async () => {
-  // create and verify user
   const email = faker.internet.email().toLocaleLowerCase();
   const password = faker.internet.password(8);
 
-  // sign up
-  await auth.signUp({
-    email,
-    password,
-  });
-
-  // get email that was sent
-  const message = await mailhog.latestTo(email);
-
-  if (!message?.html) {
-    throw new Error('email does not exists');
-  }
-
-  // get verify email link
-  const verifyEmailLink = htmlUrls({ html: message.html }).find(
-    (href: { value: string; url: string; uri: string }) => {
-      return href.url.includes('verifyEmail');
-    }
-  );
-
-  // verify email
-  await axios.get(verifyEmailLink.url, {
-    maxRedirects: 0,
-    validateStatus: (status) => {
-      return status === 302;
-    },
-  });
+  await signUpAndInUser({ email, password });
 
   const signInA = await auth.signIn({
     email,
@@ -77,38 +38,10 @@ test('change existing password', async () => {
 });
 
 test('reset password', async () => {
-  // TODO: Put in a helper function
-  // create and verify user
   const email = faker.internet.email().toLocaleLowerCase();
   const password = faker.internet.password(8);
 
-  // sign up
-  await auth.signUp({
-    email,
-    password,
-  });
-
-  // get email that was sent
-  const message = await mailhog.latestTo(email);
-
-  if (!message?.html) {
-    throw new Error('email does not exists');
-  }
-
-  // get verify email link
-  const verifyEmailLink = htmlUrls({ html: message.html }).find(
-    (href: { value: string; url: string; uri: string }) => {
-      return href.url.includes('verifyEmail');
-    }
-  );
-
-  // verify email
-  await axios.get(verifyEmailLink.url, {
-    maxRedirects: 0,
-    validateStatus: (status) => {
-      return status === 302;
-    },
-  });
+  await signUpAndVerifyUser({ email, password });
 
   // reset password
   const { error } = await auth.resetPassword({ email });
--- test/sign-out.test.ts
+++ test/sign-out.test.ts
@@ -1,61 +1,11 @@
-import { HasuraAuthClient } from '../src/index';
 import faker from 'faker';
-import createMailhogClient from 'mailhog';
-import axios from 'axios';
-
-const htmlUrls = require('html-urls');
-
-const AUTH_BACKEND_URL = 'http://localhost:1337/v1/auth';
-
-const auth = new HasuraAuthClient({
-  url: AUTH_BACKEND_URL,
-});
-
-const mailhog = createMailhogClient({
-  host: 'localhost',
-  port: 8025,
-});
+import { auth, signUpAndInUser } from './helpers';
 
 test('sign in user with email and password', async () => {
   const email = faker.internet.email().toLocaleLowerCase();
   const password = faker.internet.password(8);
 
-  // sign up
-  await auth.signUp({
-    email,
-    password,
-  });
-
-  // get email that was sent
-  const message = await mailhog.latestTo(email);
-
-  if (!message?.html) {
-    throw new Error('email does not exists');
-  }
-
-  // get verify email link
-  const verifyEmailLink = htmlUrls({ html: message.html }).find(
-    (href: { value: string; url: string; uri: string }) => {
-      return href.url.includes('verifyEmail');
-    }
-  );
-
-  // verify email
-  await axios.get(verifyEmailLink.url, {
-    maxRedirects: 0,
-    validateStatus: (status) => {
-      return status === 302;
-    },
-  });
-
-  // sign in
-  const { session, error } = await auth.signIn({
-    email,
-    password,
-  });
-
-  expect(error).toBeNull();
-  expect(session).toBeTruthy();
+  await signUpAndInUser({ email, password });
 
   expect(auth.getSession()).toBeTruthy();
 
